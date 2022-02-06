644e9fef7645f804fea218990ba41d53b50a3da1
Francisco-Borrego <fraborcar@alum.us.es>
2022-02-06 14:46:31 +0100

    Módulo bootificado

---
+++ src/main/java/es/us/isa/ideas/controller/plaintxt/PlainTXT4IdeasModuleApplication.java
@@ -0,0 +1,11 @@
+package es.us.isa.ideas.controller.plaintxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainTXT4IdeasModuleApplication {
+        public static void main(String[] args){
+            SpringApplication.run(PlainTXT4IdeasModuleApplication.class,args);
+        }
+}
